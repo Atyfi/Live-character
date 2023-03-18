823cd4f5436875bd99cd4b7ff2146a0e9cefdb5e
Atyfi <djatyfi@gmail.com>
2023-03-18 14:29:17 +0330

    Live words

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let text = document.querySelector(".text");
 let character = document.querySelector(".character");
 let limit = 120;
-
+let words = document.querySelector(".words");
 
 text.addEventListener("input",e=>{
-    let textLenght = text.value.length;
+    // character
+    let textLenght = text.value.trim().length;
     character.textContent = textLenght + "/" + limit;
 
     if(textLenght > limit){
@@ -15,4 +16,8 @@ text.addEventListener("input",e=>{
         text.style.border = "0px solid red"; 
         character.style.color = "white";         
     }
+
+    // word
+    let txt = text.value.trim();
+    words.textContent = "Words: " + txt.split(/\s+/).filter((word)=> word).length;
 })
